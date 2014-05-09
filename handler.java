c3692d4f1542f44f9742edd680ffda4ebad298c1
Danang Dwi Prasetyo <dseven182@gmail.com>
2014-05-09 22:31:50 +0700

    edit headler
    pada 49

--- handler.java
+++ handler.java
@@ -46,3 +46,62 @@ public class handler implements ActionListener{
     roosterv ro;
     tambahJadwal tb;
     
+        @Override
+    public void actionPerformed (ActionEvent e){
+        Object source = e.getSource();
+        if (view instanceof pertama){
+            pertamaf(e);
+        }else if (view instanceof user){                //halaman user
+            us = (user) view;
+            if (source.equals(us.getKembali())){
+                us.setVisible(false);
+                pertama pr1=new pertama();
+                setView(pr1);
+                setListener();
+                pr1.setVisible(true);
+            }
+        }else if (view instanceof roosterv){                   //halaman rooster
+            ro= (roosterv) view;
+            if (source.equals(ro.getKembali())){ //rooster back kembali
+                ro.setVisible(false);
+                pertama pr1= new pertama();
+                setView(pr1);
+                setListener();
+                view.setVisible(true);
+            }else if (source.equals(ro.getTambahRuang())){      //rooster tambah ruang
+                if ((ro.getTIdRuang()!=null) && (ro.getTKapasitas()>0)){
+                    boolean a2;
+                    a2=model.cekRuang(ro.getTIdRuang());
+                    if (a2==true){
+                        ruang a3= new ruang();
+                        a3.setIdRuang(ro.getTIdRuang());
+                        a3.setKapasitas(ro.getTKapasitas());
+                        model.addRuang(a3);
+                        lihatRuang(ro);
+                    }else{
+                        JOptionPane.showMessageDialog(null,"id sudah ada");
+                    }
+                }
+            }else if (source.equals(ro.getTambahJadwal())){   //rosster tambah jadwal
+                ro.setVisible(false);
+                tambahJadwal tbh= new tambahJadwal();
+                setView(tbh);
+                setListener();
+                view.setVisible(true);
+            }  
+       }else if (view instanceof tambahJadwal){                //halaman tambah jadwal
+            tb= (tambahJadwal) view;
+            if (source.equals(tb.getBack())){  //tambah jadwal back 
+                tb.setVisible(false);
+                roosterv pr1= new roosterv();
+                lihatRuang(pr1);
+                lihatJadwal(pr1);
+                setView(pr1);
+                setListener();
+                view.setVisible(true);
+            }if (source.equals(tb.gettTambah())){
+                tambahJadwalf(e);  //fungsi tambah jadwal
+            }
+       }
+}
+    
