8b9ee46f7a7a5a7f553fc88e2d2956d6186ce037
bimozaky <zakybimo@gmail.com>
2014-05-09 08:24:12 -0700

    Update sistem.java

--- sistem.java
+++ sistem.java
@@ -81,3 +81,96 @@ public class sistem {
         return a;
     }
     
+    public void loadJadwal()  throws SQLException {
+        database db=new database();
+        ResultSet rs = db.getResultSet("select *from jadwal");
+        while (rs.next()){
+            jadwal j = new jadwal();
+            j.setKdJadwal(rs.getString(1));
+            j.setJam(rs.getInt(2));
+            j.setMataKuliah(rs.getString(3));
+            j.setDosen(rs.getString(4));
+            j.setHari(rs.getInt(5));
+            j.setNamaKelas(rs.getString(6));
+            j.setKdRuang(rs.getString(7));
+            getDaftarJadwal().add(j);
+        }
+        rs.close();
+    }
+    public void loadRuang () throws SQLException{
+        database db=new database();
+        ResultSet rs = db.getResultSet("select *from ruang");
+        while (rs.next()){
+            ruang r= new ruang();
+            r.setIdRuang(rs.getString(1));
+            r.setKapasitas(rs.getInt(2));
+            getDaftarRuang().add(r);
+        }
+        rs.close();
+    }
+    public void loadRuangJadwal() throws SQLException{
+        for (int i=0; i<getDaftarRuang().size();i++){
+           for (int j=0; j<getDaftarJadwal().size();j++){
+               if (getDaftarJadwal().get(i).getKdRuang().equals(getDaftarRuang().get(j).getIdRuang())){
+                    getDaftarRuang().get(i).addJadwal(getDaftarJadwal().get(j));
+               }
+           }
+        }
+    }
+    
+    public void loadRooster() throws SQLException{
+        database db=new database();
+        ResultSet rs = db.getResultSet("select *from rooster");
+        while (rs.next()){
+            rooster a= new rooster();
+            a.setIdPegawai(rs.getString(1));
+            a.setUsername(rs.getString(2));
+            a.setPassword(rs.getString(3));
+            a.setNama(rs.getString(4));
+            daftarRooster.add(a);
+        }
+        rs.close();
+    }
+    
+    public jadwal carijadwal(String a) {
+        for (int i=0; i<getDaftarJadwal().size();i++){
+            if (a.equals(getDaftarJadwal().get(i).getKdJadwal())){
+                return getDaftarJadwal().get(i);
+            }
+        }
+        return null;
+    }
+    
+    public ruang cariRuang (String a){
+        for (int i=0;i<getDaftarJadwal().size();i++){
+            if (a.equals(getDaftarRuang().get(i).getIdRuang())){
+                return getDaftarRuang().get(i);
+            }
+        }
+        return null;
+    }
+    
+       public boolean cekloggin(String user, String Pass){
+            boolean a= false;
+            for (int i =0 ; i<daftarRooster.size();i++){
+                if ((daftarRooster.get(i).getUsername().equals(user)) && (daftarRooster.get(i).getPassword().equals(Pass))){
+                    a=true;
+                }
+            }
+            return a;
+       }
+
+    /**
+     * @return the daftarJadwal
+     */
+    public ArrayList<jadwal> getDaftarJadwal() {
+        return daftarJadwal;
+    }
+
+    /**
+     * @return the daftarRuang
+     */
+    public ArrayList<ruang> getDaftarRuang() {
+        return daftarRuang;
+    }
+}
